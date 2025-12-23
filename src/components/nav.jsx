c77a9611d009f7b32659a0e930972bf1575981db
Nitish Maurya <nitishmaurya2255@gmail.com>
2025-12-23 15:13:02 +0530

    login added

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import "./nav.css";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   const [open, setOpen] = useState(false);
 
   return (
     <nav className="navbar h-[100px]">
-      <div className="logo text-4xl font-extrabold">Apna-Tiffin</div>
+      <div className="logo text-4xl font-extrabold p-10">Apna-Tiffin</div>
 
       <ul className={`nav-list ${open ? "active" : ""}`}>
         <li>Home</li>
@@ -14,7 +15,9 @@ function Navbar() {
         <li>Services</li>
         <li>Contact</li>
       </ul>
-
+     <Link to="/login">
+      <button>Login</button>
+     </Link>
       <div className="hamburger" onClick={() => setOpen(!open)}>
         ☰
       </div>
